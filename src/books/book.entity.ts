0f961534d81fc9cdfdcac8b154513d50f1806bc1
Johan Hdez <hernandez.gomez29202@gmail.com>
2024-04-14 21:23:39 -0500

    create tables

---
+++ src/books/book.entity.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'books'})
+export class Book {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    @Column({nullable: true})
+    desc: string
+
+    @Column()
+    price: number
+
+    @Column()
+    author: string
+
+    @Column({type: 'datetime'})
+    pubDate: Date
+
+    @Column()
+    inStock: boolean
+}
