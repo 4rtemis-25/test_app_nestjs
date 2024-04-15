ade3ceee43ea604da0b3c18a89de3d521111ebb5
Johan Hdez <hernandez.gomez29202@gmail.com>
2024-04-14 21:05:15 -0500

    firts configuration bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+       type: 'mysql',
+       host: 'localhost',
+       port: 3306,
+       username: 'root',
+       password: '',
+       database: 'crud_books_db',
+       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+       synchronize: true
+      }
+    ),
+    BooksModule
+  ],
   controllers: [],
   providers: [],
 })
